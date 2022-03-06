e8f8cf19793c01dcbd1737ce59bed6f6af0d6216
Jay B <79182784+JayBee-K@users.noreply.github.com>
2022-03-06 19:17:33 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,25 @@
 let windowWidth = $(window).width();
 const handleTouchMove = function (ev) {
-	ev.preventDefault();
+    ev.preventDefault();
 }
 
 $(function () {
+    $('#popupDetailProduct').on('hide.bs.modal', function () {
+        $(this).addClass('modal-hide');
+    }).on('hidden.on.modal', function () {
+        $('body').removeClass('modal-overlay-white');
+    }).on('show.bs.modal', function () {
+        $(this).removeClass('modal-hide');
+        $('body').addClass('modal-overlay-white');
+    });
+    $('.btn-quick_view').click(function (e) {
+        $('#popupDetailProduct').modal('show');
+    });
+
+    AOS.init({
+        easing: "ease-out-quad",
+        once: !0,
+        offset: 60,
+        disableMutationObserver: !0
+    });
 });
