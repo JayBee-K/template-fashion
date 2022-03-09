69efe1d4426b81f8f88d18404c8c8648bfd6f624
JayBee-K <79182784+JayBee-K@users.noreply.github.com>
2022-03-09 13:48:27 +0700

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,44 @@
 let windowWidth = $(window).width();
 const handleTouchMove = function (ev) {
-    ev.preventDefault();
+	ev.preventDefault();
+}
+
+const handlePopupDetailProduct = function () {
+	$('#popupDetailProduct').on('hide.bs.modal', function () {
+		$(this).addClass('modal-hide');
+	}).on('hidden.on.modal', function () {
+		$('body').removeClass('modal-overlay-white');
+	}).on('show.bs.modal', function () {
+		$(this).removeClass('modal-hide');
+		$('body').addClass('modal-overlay-white');
+	});
+
+	$('.btn-quick_view').click(function (e) {
+		const previewThumb = new Swiper('#preview-thumb', {
+			spaceBetween: 15,
+			slidesPerView: 4,
+			freeMode: true,
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+			direction: "vertical",
+		});
+		const previewPhoto = new Swiper('#preview-photo', {
+			thumbs: {
+				swiper: previewThumb,
+			},
+		});
+
+		$('#popupDetailProduct').modal('show');
+	});
 }
 
 $(function () {
-    $('#popupDetailProduct').on('hide.bs.modal', function () {
-        $(this).addClass('modal-hide');
-    }).on('hidden.on.modal', function () {
-        $('body').removeClass('modal-overlay-white');
-    }).on('show.bs.modal', function () {
-        $(this).removeClass('modal-hide');
-        $('body').addClass('modal-overlay-white');
-    });
-    $('.btn-quick_view').click(function (e) {
-        $('#popupDetailProduct').modal('show');
-    });
+	handlePopupDetailProduct();
 
-    AOS.init({
-        easing: "ease-out-quad",
-        once: !0,
-        offset: 60,
-        disableMutationObserver: !0
-    });
+	AOS.init({
+		easing: "ease-out-quad",
+		once: !0,
+		offset: 60,
+		disableMutationObserver: !0
+	});
 });
